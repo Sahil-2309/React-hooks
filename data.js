436e42989b7d6f8f7f7c0cd26ea9cc04238ff131
Sahil Arora <91737464+Sahil-2309@users.noreply.github.com>
2023-02-21 23:18:20 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,8 @@
+export const data = [
+  { id: 1, name: 'sahil' },
+  { id: 2, name: 'sahil1' },
+  { id: 3, name: 'sahil3' },
+  { id: 4, name: 'sahil4' },
+  { id: 5, name: 'sahil5' },
+]
+export default data
